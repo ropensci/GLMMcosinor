df08cea9d23019d174297f9347d351ac943f649a
oliverjayasinghe <112680762+oliverjayasinghe@users.noreply.github.com>
2023-01-06 14:50:21 +1000

    Added some test_that

--- tests/testthat/test-amp-acro.R
+++ tests/testthat/test-amp-acro.R
@@ -1,15 +1,14 @@
 test_that("multiplication works", {
   amp.acro(
-    time_col = time, n_components = 1, group = c(NA, "X"), period = c(24, 12), .data = vitamind,
-    .formula = Y ~ X + amp.acro(time, n_components = 2, group = c(NA, "X"), period = c(24, 12)),
-    test = TRUE
+    time_col = time, n_components = 2, group = "X", period = c(24, 12), .data = vitamind,
+    .formula = Y ~ X + amp.acro(time, n_components = 2, group = "X", period = c(24, 12))
   )
 
   f <- Y ~ X + amp.acro(time, n_components = 2, group = c(NA, "X"))
 
   cosinor.glmm(Y ~ X + amp.acro(time, n_components = 2, group = c(NA, "X"), period = c(12, 24)), data = vitamind)
+  expect(ok = TRUE, "unsuccessful multiplication of group to match n_components")
 })
-
 
 test_that("bad inputs return useful errors", {
   f <- function() {
@@ -35,4 +34,16 @@ test_that("bad inputs return useful errors", {
   }
 
   expect_error(f(), regexp = "period > 0") # negative period
+
+  data(vitamind)
+  vitamind_mod <- vitamind
+  vitamind_mod$time <- as.factor(rbinom(length(vitamind$time),1,0.5))
+  f <- function() {
+    amp.acro(
+      time_col = time, n_components = 1, group = "X", period = 12,
+      .data = vitamind_mod,
+      .formula = Y ~ X + amp.acro(time, n_components = 1, group = "X", period = 12)
+    )
+  }
+  expect_error(f(), regexp = "ttt is not a numeric or integer vector")
 })
--- tests/testthat/test-update_formula_and_data.R
+++ tests/testthat/test-update_formula_and_data.R
@@ -1,23 +1,33 @@
 test_that("assess formula manipulation", {
   res <-
     update_formula_and_data(
-      formula = Y ~ group + amp.acro(time, n_components = 2, group = "X", period = c(12, 10)),
+      formula = Y ~ X + amp.acro(time, n_components = 2, group = "X", period = c(12, 10)),
       data = vitamind
     )
-
-  expect_true(all.equal(res$formula, Y ~ X + rrr1 + sss1 + rrr2 + sss2 + X:rrr1 + X:sss1 + X:rrr2 + X:sss2))
-
-
-
-  dat <- vitamind
-  dat$ID <- sample(LETTERS[1:5], size = nrow(dat), replace = TRUE)
-  res <- update_formula_and_data(
-    formula = Y ~ group + amp.acro(time, n_components = 1, group = "X", period = 24),
-    data = dat
-  )
-  expected_formula <- Y ~ X + rrr1 + sss1 + X:rrr1 + X:sss1 + (rrr1 + sss1 + X:rrr1 + X:sss1 | ID)
-  expected_formula <- Y ~ X + rrr1 + sss1 + X:rrr1 + X:sss1 + (rrr1 + sss1 | ID) + (X:rrr1 + X:sss1 | ID)
-
-  Y <- 1 + group + (1 | ID)
-  Y <- 1 + group + (1 + group | ID)
+  expect_true(all.equal(res$newformula, Y ~ X + X:rrr1 + X:sss1 + X:rrr2 + X:sss2))
 })
+
+test_that("warning for missing data", {
+  data(vitamind)
+  vitamind$Y[1] <- NA
+  res <- function() {
+    update_formula_and_data(
+      formula = Y ~ X + amp.acro(time, n_components = 2, group = "X", period = c(12, 10)),
+      data = vitamind, quietly = FALSE
+    )
+  }
+  expect_message(res())
+})
+
+#not sure if this is relevant?
+#  dat <- vitamind
+#  dat$ID <- sample(LETTERS[1:5], size = nrow(dat), replace = TRUE)
+#  res <- update_formula_and_data(
+#    formula = Y ~ X + amp.acro(time, n_components = 1, group = "X", period = 24),
+#    data = dat
+#  )
+#  expected_formula <- Y ~ X + X:rrr1 + X:sss1 + (X:rrr1 + X:sss1 | ID)
+#
+#  Y <- 1 + X + (1 | ID)
+
+
