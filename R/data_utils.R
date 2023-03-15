5eca798703ae3974ed787d38bcb9cf3b2b71b947
oliverjayasinghe <112680762+oliverjayasinghe@users.noreply.github.com>
2023-03-15 23:04:29 +1000

    Move get_new_coefs to data_utils as a function

--- R/data_utils.R
+++ R/data_utils.R
@@ -383,3 +383,51 @@ validate_ci_level <- function(ci_level) {
     stop("'ci_level' must be a single numeric value in [0, 1].")
   }
 }
+
+# calculate the parameters from the raw estimates
+get_new_coefs <- function(coefs, vec_rrr, vec_sss, n_components) {
+  r.coef <- NULL
+  s.coef <- NULL
+  mu.coef <- NULL
+  mu_inv <- rep(0, length(names(coefs)))
+
+
+  # Get a Boolean vector for rrr, sss, and mu. This will be used to extract
+  # the relevant raw parameters from the raw coefficient model output
+  for (i in 1:n_components) {
+    r.coef[[i]] <- grepl(paste0(vec_rrr[i]), names(coefs))
+    s.coef[[i]] <- grepl(paste0(vec_sss[i]), names(coefs))
+
+    mu_inv_carry <- r.coef[[i]] + s.coef[[i]] # Keep track of non-mesor terms
+    mu_inv <- mu_inv_carry + mu_inv # Ultimately,every non-mesor term will be true
+  }
+
+  mu.coef <- c(!mu_inv) # invert 'mu_inv' to get a Boolean vector for mesor terms
+  r.coef <- (t(matrix(unlist(r.coef), ncol = length(r.coef)))) # a matrix of rrr coefficients
+  s.coef <- (t(matrix(unlist(s.coef), ncol = length(s.coef)))) # a matrix of sss coefficients
+
+  # Calculate the parameter estimates for all components
+  amp <- NULL
+  acr <- NULL
+  for (i in 1:n_components) {
+    beta.s <- coefs[s.coef[i, ]]
+    beta.r <- coefs[r.coef[i, ]]
+
+    groups.r <- c(beta.r[1], beta.r[which(names(beta.r) != names(beta.r[1]))])
+    groups.s <- c(beta.s[1], beta.s[which(names(beta.s) != names(beta.s[1]))])
+
+    amp[[i]] <- sqrt(groups.r^2 + groups.s^2)
+    names(amp[[i]]) <- gsub(vec_rrr[i], paste0("amp", i), names(beta.r))
+
+    acr[[i]] <- -atan2(groups.s, groups.r)
+    names(acr[[i]]) <- gsub(vec_sss[i], paste0("acr", i), names(beta.s))
+  }
+  new_coefs <- c(coefs[mu.coef], unlist(amp), unlist(acr))
+  # if n_components = 1, then print "amp" and "acr" rather than "amp1", "acr1"
+  if (n_components == 1) {
+    names(amp[[1]]) <- gsub(vec_rrr[1], "amp", names(beta.r))
+    names(acr[[1]]) <- gsub(vec_sss[1], "acr", names(beta.s))
+    new_coefs <- c(coefs[mu.coef], unlist(amp), unlist(acr))
+  }
+  new_coefs
+}
