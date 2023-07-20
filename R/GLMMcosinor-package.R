611801d8200fc3afdb9fd1b58883eb1604662eb4
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2023-07-20 11:03:22 +1000

    correct cases of "GLMMCosinor" to "GLMMcosinor"

--- R/GLMMcosinor-package.R
+++ R/GLMMcosinor-package.R
@@ -11,7 +11,7 @@
 
 
 #' @keywords internal
-#' @name GLMMCosinor
+#' @name GLMMcosinor
 #' @title GLMMcosinor: A package for fitting generalized linear mixed-effects models with cosinor terms
 #' @description This package provides functions for fitting generalized linear mixed-effects models with cosinor terms. Cosinor terms can be used to model periodic variations in time series data, and are particularly useful in chronobiology and sleep research. The package uses the flexible GLMMtmb framework that allows users to specify a wide range of data families, and provides tools for visualization.
 #' @keywords GLMM, cosinor, mixed-effects models, time series, chronobiology, sleep research
