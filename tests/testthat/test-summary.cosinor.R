4fc4856cf4896d556f3f1a6dea19f07600ca612d
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2024-08-19 14:33:43 +1000

    test summary.cosinor from fixture model objects

--- tests/testthat/test-summary.cosinor.R
+++ tests/testthat/test-summary.cosinor.R
@@ -1,23 +1,25 @@
 test_that("summary print works", {
-  withr::local_seed(50)
-  object <- cglmm(
-    vit_d ~ amp_acro(time, group = "X", period = 12),
-    data = vitamind
-  )
-  print_obj <- summary(object)
-  expect_no_error(print_obj)
-  expect_snapshot(print(print_obj, digits = 2))
+  # withr::local_seed(50)
+  # simple_model <- cglmm(
+  #   vit_d ~ amp_acro(time, group = "X", period = 12),
+  #   data = vitamind
+  # )
+  # saveRDS(simple_model, test_path("fixtures", "simple_model.rds"))
+  # multi_model <- cglmm(
+  #   vit_d ~ amp_acro(time, group = "X", period = 12),
+  #   dispformula = ~ amp_acro(time, group = "X", period = 12),
+  #   ziformula = ~ amp_acro(time, group = "X", period = 12),
+  #   data = vitamind
+  # )
+  # saveRDS(multi_model, test_path("fixtures", "multi_model.rds"))
 
+  simple_model <- readRDS(test_path("fixtures", "simple_model.rds"))
+  multi_model <- readRDS(test_path("fixtures", "multi_model.rds"))
+
+  print_obj <- summary(simple_model)
+  expect_snapshot(print(print_obj, digits = 2))
   expect_true(inherits(print_obj, "cglmmSummary"))
 
-  # test the dispersion and zeroinflation summaries
-  object_2 <- cglmm(
-    vit_d ~ amp_acro(time, group = "X", period = 12),
-    dispformula = ~ amp_acro(time, group = "X", period = 12),
-    ziformula = ~ amp_acro(time, group = "X", period = 12),
-    data = vitamind
-  )
-  print_obj <- summary(object_2)
-  expect_no_error(print_obj)
+  print_obj <- summary(multi_model)
   expect_snapshot(print(print_obj, digits = 2))
 })
