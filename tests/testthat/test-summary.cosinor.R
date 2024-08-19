fa219c6f918864c947d301c5ab9e67573f99d29d
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2024-08-19 14:12:42 +1000

    update tests and snaps

--- tests/testthat/test-summary.cosinor.R
+++ tests/testthat/test-summary.cosinor.R
@@ -1,39 +1,23 @@
 test_that("summary print works", {
-  withr::with_seed(
-    50,
-    {
-      data(vitamind)
-      object <- cglmm(
-        vit_d ~ amp_acro(time,
-          group = "X",
-          period = 12
-        ),
-        data = vitamind
-      )
-      print_obj <- summary(object)
-      testthat::expect_no_error(print_obj)
-      testthat::expect_snapshot_output(print(print_obj, digits = 2))
-
-      testthat::expect_true(inherits(print_obj, "cglmmSummary"))
-
-      # test the dispersion and zeroinflation summaries
-      object_2 <- cglmm(
-        vit_d ~ amp_acro(time,
-          group = "X",
-          period = 12
-        ),
-        dispformula = ~ amp_acro(time,
-          group = "X",
-          period = 12
-        ), ziformula = ~ amp_acro(time,
-          group = "X",
-          period = 12
-        ),
-        data = vitamind
-      )
-      print_obj <- summary(object_2)
-      testthat::expect_no_error(print_obj)
-      testthat::expect_snapshot_output(print(print_obj, digits = 2))
-    }
+  withr::local_seed(50)
+  object <- cglmm(
+    vit_d ~ amp_acro(time,group = "X",period = 12),
+    data = vitamind
   )
+  print_obj <- summary(object)
+  expect_no_error(print_obj)
+  expect_snapshot(print(print_obj, digits = 2))
+
+  expect_true(inherits(print_obj, "cglmmSummary"))
+
+  # test the dispersion and zeroinflation summaries
+  object_2 <- cglmm(
+    vit_d ~ amp_acro(time,group = "X",period = 12),
+    dispformula = ~ amp_acro(time,group = "X",period = 12),
+    ziformula = ~ amp_acro(time,group = "X",period = 12),
+    data = vitamind
+  )
+  print_obj <- summary(object_2)
+  expect_no_error(print_obj)
+  expect_snapshot(print(print_obj, digits = 2))
 })
